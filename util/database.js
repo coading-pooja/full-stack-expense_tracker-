d0113156a42463417a1b1b369fec6fdee8a6dd3d
Pooja Goyal <poojagoyal5106@gmail.com>
2023-10-14 16:19:09 +0530

    my sql data into .env

--- util/database.js
+++ util/database.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expense-tracker-full-stack', 'root', process.env.MYSQL_PASSWORD, {
+// const sequelize = new Sequelize('expense-tracker-full-stack', 'root', process.env.MYSQL_PASSWORD, {
+  const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+
   dialect: 'mysql',
   host: 'localhost',
  // logging: console.log
