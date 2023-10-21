a63ab841172e3401968f425ffc15f1c61d432486
Pooja Goyal <poojagoyal5106@gmail.com>
2023-10-21 19:09:08 +0530

    AWSdeploy

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
   const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
 
   dialect: 'mysql',
-  host: 'localhost',
+  host: '13.127.157.193',
  // logging: console.log
 });
 
